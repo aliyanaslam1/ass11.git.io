aed9756f47e957cd037d84c8cf34a9df3a9654fa
aliyanaslam1 <118340179+aliyanaslam1@users.noreply.github.com>
2022-11-16 12:58:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+alert("Hello World");
+alert("error! please enter a valid password");
+alert("Welcome to js land.. \n happy coding...");
+alert("welcome to js land");
+alert("happy coding");
+window.alert("hello... i can run js through my web browser");
+
+// chap no 2
+var username;
+var myname = "Aliyan Aslam";
+var message = "Hello World";
+alert(message);
+ 
+var name1 = "Aliyan";
+var roll = 15;
+var course = "web and mobile application";
+
+alert(name1);
+alert(roll);
+alert(course);
+
+var food = "pizza";
+alert(food);
+food = "pizz";
+alert(food);
+food = "piz";
+alert(food);
+food = "pi";
+alert(food);
+food = "p";
+alert(food);
+
+
+var email = "aliyanaslam1234567@gmail.com";
+alert(email);
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+
+document.write("yah !! i can done the same work of html using java script");
+var str = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(str);
